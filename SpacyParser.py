488b05c72c6cb25e6632f2e41674a3c2383c24a7
Tony Dfouny <tony_dfouny@hotmail.com>
2023-03-20 23:43:31 +0200

    ideas files + sapcy fcts

--- SpacyParser.py
+++ SpacyParser.py
@@ -1,13 +1,74 @@
 import spacy
 nlp = spacy.load("en_core_web_sm")
-about_text = (
-    "I am"
- )
-about_doc = nlp(about_text)
-for token in about_doc:
+sentence ="They are eating an apple and an orange.Tony is sleeping"
+sentence2="Tony Dfouny,the kids and I are sleeping"
+sentence3='There are many factors affecting teacher retention'
+doc = nlp(sentence2)
+doc2=nlp(sentence3)
+
+
+
+# for token in doc2:
+#     print(token.text, "\t", token.dep_, "\t",
+#     spacy.explain(token.dep_))
+#     ancestors = [t.text for t in token.ancestors]
+#     print(ancestors)
+
+# for token in doc:
+#     print(
+#         f"""
+#     TOKEN: {str(token)}
+#     =====
+#     TAG: {str(token.tag_):10} POS: {token.pos_}
+#     EXPLANATION: {spacy.explain(token.tag_)}""")
+#     if token.dep_=='nsubj':
+#         ancestors = [t.text for t in token.ancestors]
+#         print(token.text,'verb = ',ancestors)
+
+
+
+# for token in doc:
+#     print(token.text)
+#     for t in token.children:
+#         if t.dep_=='nsubj':
+#             children=[t.text]
+#             print(children)
+
+
+
+
+
+
+
+
+
+
+
+##############ORIGINAL###################
+
+#########TAGS#########
+for token in doc:
      print(
          f"""
 TOKEN: {str(token)}
 =====
 TAG: {str(token.tag_):10} POS: {token.pos_}
 EXPLANATION: {spacy.explain(token.tag_)}""" )
+
+##########DEPENDENCY#########
+for token in doc:
+    print(token.text, "\t", token.dep_, "\t",
+    spacy.explain(token.dep_))
+
+########ANCESTORS###########
+# for token in doc:
+#     print(token.text)
+#     ancestors = [t.text for t in token.ancestors]
+#     print(ancestors)
+
+
+########CHILDREN##########
+# for token in doc:
+#     print(token.text)
+#     children = [t.text for t in token.children]
+#     print(children)
--- english.py
+++ english.py
@@ -1,33 +1,33 @@
-import nltk
-from nltk import pos_tag, word_tokenize, RegexpParser
-from nltk.corpus import treebank
-sentence = """They are eating an apple and he is sleeping"""
-
-tokens = nltk.word_tokenize(sentence)
-print("tokens = \n",tokens)
-
-tagged = nltk.pos_tag(tokens)
-print("tagged = \n",tagged)
-singular=['I','You','HE','SHE','IT']
-plural=['WE','You','THEY']
-def Customtag(tagged):
-    liste=[]
-    customlist=[]
-    for i in tagged:
-        if i[0].upper() in singular:
-            liste=list(i)
-            liste[1] = 'PRPS'
-            customlist.append(liste)
-        elif i[0].upper() in plural:
-            liste = list(i)
-            liste[1] = 'PRPP'
-            customlist.append(liste)
-        else:
-            liste = list(i)
-            customlist.append(liste)
-    print('LIsteeeeee=',liste)
-    return customlist
-print('Custommmmm=',Customtag(tagged))
+# import nltk
+# from nltk import pos_tag, word_tokenize, RegexpParser
+# from nltk.corpus import treebank
+# sentence = """They are eating an apple and he is sleeping"""
+#
+# tokens = nltk.word_tokenize(sentence)
+# print("tokens = \n",tokens)
+#
+# tagged = nltk.pos_tag(tokens)
+# print("tagged = \n",tagged)
+# singular=['I','You','HE','SHE','IT']
+# plural=['WE','You','THEY']
+# def Customtag(tagged):
+#     liste=[]
+#     customlist=[]
+#     for i in tagged:
+#         if i[0].upper() in singular:
+#             liste=list(i)
+#             liste[1] = 'PRPS'
+#             customlist.append(liste)
+#         elif i[0].upper() in plural:
+#             liste = list(i)
+#             liste[1] = 'PRPP'
+#             customlist.append(liste)
+#         else:
+#             liste = list(i)
+#             customlist.append(liste)
+#     print('LIsteeeeee=',liste)
+#     return customlist
+# print('Custommmmm=',Customtag(tagged))
 #print(('PRP') in tagged[0:][0:][0:])
 #
 # entities = nltk.chunk.ne_chunk(tagged)
@@ -78,16 +78,17 @@ print('Custommmmm=',Customtag(tagged))
 # t = treebank.parsed_sents('wsj_0001.mrg')[0]
 # t.draw()
 
-# from Parser import Parser
-#
-#
-#
-# parser = Parser(model_path=r"C:\Users\tony_\Downloads\stanford-corenlp-4.2.0-models-english\edu\stanford\nlp\models\lexparser\englishPCFG.caseless.ser.gz",
-#                 path_to_jar=r"C:\Users\tony_\Downloads\stanford-parser-4.2.0\stanford-parser-full-2020-11-17\stanford-parser.jar",
-#                 path_to_models_jar=r"C:\Users\tony_\Downloads\stanford-parser-4.2.0\stanford-parser-full-2020-11-17\stanford-parser-4.2.0-models.jar")
-#
-# result=parser.parse_sentence("They are eating an apple and an orange and he is sleeping")
-# #result=parser.parse_sentence("They are drinking and they are singing")
-# print(result)
-# #parser.tree_draw()
-# parser.tree_sentence()
+from Parser import Parser
+
+
+
+parser = Parser(model_path=r"C:\Users\tony_\Downloads\stanford-corenlp-4.2.0-models-english\edu\stanford\nlp\models\lexparser\englishPCFG.caseless.ser.gz",
+                path_to_jar=r"C:\Users\tony_\Downloads\stanford-parser-4.2.0\stanford-parser-full-2020-11-17\stanford-parser.jar",
+                path_to_models_jar=r"C:\Users\tony_\Downloads\stanford-parser-4.2.0\stanford-parser-full-2020-11-17\stanford-parser-4.2.0-models.jar")
+
+result=parser.parse_sentence("They are eating an apple and an orange and he is sleeping")
+#result=parser.parse_sentence("They are drinking and they are singing")
+print(result)
+parser.tree_draw()
+#parser.tree_sentence()
+
