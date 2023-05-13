58cbcdd906bfb9211e0f5fabb0f9002daf021d02
Tony Dfouny <TonyDfouny@Joelles-MacBook-Pro.local>
2023-05-13 19:44:24 +0300

    main os change

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ LanguageTranslator()
 #
 # print(ArPhoeTranslator.ArPhoeTranslator('eat','Offline'))
 
+##TRY IT TO MAKE PROG WORK ON ANY PC##
+from pathlib import Path
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+import os
+ROOT_DIR = os.path.abspath(os.curdir)
+
+import sys
+print(sys.path[1])
